aad1975cd01c1d5d49cfdcd1ce7df58a9f5a578a
sm13121999 <45043072+sm13121999@users.noreply.github.com>
2018-11-21 14:22:29 +0000

    Update hello_world2.py

--- hello_world2.py
+++ hello_world2.py
@@ -1,6 +1,10 @@
-print ('hello world')
-x=1
-while x<10:
-    print (x)
-    x=x+1
+def HW (x):
+    print ('hello world')   
+    while x<10:
+        print (x)
+        x=x+1
+
+#main
+x=0
+HW (x)
 
